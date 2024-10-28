26e66fa699044b647b1b704ef29b7b7f07383cfb
Manav <manav18gadhiya@gmail.com>
2024-10-28 17:10:53 +0530

    add: opengraph images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export const metadata: Metadata = {
     siteName: `${DATA.name}`,
     locale: "en_US",
     type: "website",
+    images: "/me.jpg",
   },
   robots: {
     index: true,
@@ -42,6 +43,7 @@ export const metadata: Metadata = {
   twitter: {
     title: `${DATA.name}`,
     card: "summary_large_image",
+    images: "/me.jpg",
   },
   verification: {
     google: "",
