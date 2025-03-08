c743cb9867d77c5e2fc278730557ee00ba645987
Manav <manav18gadhiya@gmail.com>
2025-03-08 13:42:19 +0530

    update bio

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
 
+type Feature = {
+  text: string;
+};
+
 type ProjectProps = {
   name: string;
   description: string;
   sourceLink: string;
   websiteLink?: string;
+  npmLink?: string;
+  features?: Feature[];
+  published?: boolean;
 };
 
 export function Project({
@@ -12,21 +19,44 @@ export function Project({
   description,
   sourceLink,
   websiteLink,
+  npmLink,
+  features,
+  published,
 }: ProjectProps) {
   return (
     <div className="flex flex-col gap-2 mb-6">
-      <div className="flex gap-4 items-center">
-        <h1 className="text-lg font-semibold">{name}</h1>
-        <Link href={sourceLink} target="_blank">
-          <p className="underline">Source</p>
-        </Link>
-        {websiteLink && (
-          <Link href={websiteLink} target="_blank">
-            <p className="underline">Website</p>
-          </Link>
+      <div className="flex flex-wrap gap-4 items-center">
+        <h1 className="text-2xl font-semibold">{name}</h1>
+        {published && (
+          <span className="bg-green-700 text-white text-xs px-2 py-0.5 rounded-full">
+            Published
+          </span>
         )}
+        <div className="flex gap-4">
+          <Link href={sourceLink} target="_blank">
+            <p className="underline">Source</p>
+          </Link>
+          {websiteLink && (
+            <Link href={websiteLink} target="_blank">
+              <p className="underline">Website</p>
+            </Link>
+          )}
+          {npmLink && (
+            <Link href={npmLink} target="_blank">
+              <p className="underline">npm</p>
+            </Link>
+          )}
+        </div>
       </div>
       <p>{description}</p>
+
+      {features && features.length > 0 && (
+        <ul className="list-disc pl-5 mt-2">
+          {features.map((feature, index) => (
+            <li key={index}>{feature.text}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,22 +5,52 @@ export default function Page() {
     <section>
       <Project
         name="Rate Limiting Library"
-        description="A highly configurable & performant rate-limiting library for node.js"
+        description="A highly flexible rate limiting solution for Node.js applications."
         sourceLink="https://github.com/0xRadioAc7iv/rate-limiter"
+        npmLink="https://www.npmjs.com/package/@radioac7iv/rate-limiter"
         websiteLink="https://rate-limiter.0xradioactiv.xyz/"
+        published={true}
+        features={[
+          {
+            text: "Multi-framework Support: Seamlessly integrates with Express, Fastify & NestJS",
+          },
+          {
+            text: "Storage Options: Compatible with Redis, MongoDB & In-Memory stores",
+          },
+          {
+            text: "Quality Assured: 90% test coverage with comprehensive tests",
+          },
+          {
+            text: "Advanced Features: Dynamic rate limiting, configurable options, optional logging",
+          },
+        ]}
+      />
+      <Project
+        name="E-Commerce Platform MVP"
+        description="A full-featured e-commerce platform with modern capabilities."
+        sourceLink="https://github.com/0xRadioAc7iv/E-Commerce-Platform/"
+        // websiteLink=""
+        features={[
+          { text: "JWT Auth w/ Password Reset using Email" },
+          { text: "Wishlist and Cart functionality" },
+          { text: "Stripe Integration for payments" },
+          { text: "Search Functionality" },
+        ]}
       />
       <Project
         name="Mock Data Generator"
         description="The Mock Data Generator is a powerful and user-friendly tool designed for developers to quickly generate and interact with mock data."
         sourceLink="https://github.com/0xRadioAc7iv/mock-data-generator"
         websiteLink="https://mock-data-generator-mu.vercel.app/"
+        features={[
+          {
+            text: "Copy Data as SQL/MongoDB Insert Query",
+          },
+          {
+            text: "Download Data as CSV & JSON",
+          },
+        ]}
       />
-      <Project
-        name="E-Commerce Platform"
-        description="A feature-rich e-commerce platform with authentication, payment integration, and wishlist/cart functionalities."
-        sourceLink="https://github.com/0xRadioAc7iv/E-Commerce-Platform"
-      />
-      <div>More to come...</div>
     </section>
   );
 }
